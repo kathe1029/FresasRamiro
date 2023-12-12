ff06679c2312be36129ab0262a5602493962eead
Daniel Amaya <amayadaniel677@gmail.com>
2023-12-12 08:24:20 -0500

    inicio del pryecto con carpetas organizadas

---
+++ view/layout/nav.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header-nav">
+        <img src="resource/img/logo/logofresas.png" alt="logo don ramiro" class="logo" />
+        <button class="abrir-menu" id="abrir-menu">
+            <i class="bi bi-list"></i>
+        </button>
+
+        <nav class="nav">
+            <ul class="nav-list">
+                <button class="cerrar-menu" id="cerrar-menu">
+                    <i class="bi bi-x-lg"></i>
+                </button>
+                <div class="nav-items-left">
+                    <li><a href="#section-inicio">Inicio</a></li>
+                    <li><a href="#section-recetas">Recetas</a></li>
+                    <li><a href="#section-contacto">Contacto</a></li>
+                </div>
+
+                <div class="nav-buttons">
+                    <li><a href="">Registro</a></li>
+                    <li>
+                        <a href="">Ingresar<i class="bi bi-box-arrow-in-right"></i></a>
+                    </li>
+                </div>
+            </ul>
+        </nav>
+    </header>
+    <script src="../js/nav.js"></script>
+</body>
+
+</html>
